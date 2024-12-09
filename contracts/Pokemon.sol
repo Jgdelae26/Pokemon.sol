7f438ed74dd3d0b152fdd191ddc32d243f90cb2b
Jgdelae26 <jgdelae26@gmail.com>
2024-12-09 01:27:28 +0100

    Se guarda el nombre del entrenador qeu es el que accede al contrato y su nombre determinara las stats iniciales del pokemon principal en funcion del elemento que escoja. Cada entrenador solo podra escoger un inicial por primera vez.

--- contracts/Pokemon.sol
+++ contracts/Pokemon.sol
@@ -9,14 +9,18 @@ contract ZombieFactory {
     //para generar eventos a nivel de web3
     event NewPokemon(uint pokemonId, string name, uint poder);
 
-    uint poderDigits = 16;
+    uint poderDigits = 5;
     //Nos sirve para limitar el tamaño de la variable poder a 16 digitos
     uint poderModulus = 10 ** poderDigits;
 
     struct Pokemon {
         string name;
-        string tipo;
-        uint poder;
+        string elemento;
+        uint ataque;
+        uint defensa;
+        uint ataqueEspecial;
+        uint defensaEspecial;
+        uint velocidad;
     }
 
     //Array de pokemons
@@ -27,24 +31,50 @@ contract ZombieFactory {
     mapping (uint => address) public pokemonToOwner;
     mapping (address => uint) ownerPokemonConunt;
 
-    function _createPokemon(string _name, string _tipo, uint _poder) private {
-        uint id = pokemons.push(Pokemon(_name, _tipo, _poder)) - 1;
+    //Mapping para guardar el nombre del entrenador
+    mapping (address => string) public trainerName;
+
+    function _createPokemon(string memory _name, string memory _elemento, uint  _poder, string calldata _trainerName) private {
+        uint ataque = _poder % 10;
+        uint defensa = (_poder/10) % 10;
+        uint ataqueEspecial = (_poder/100) % 10;
+        uint defensaEspecial = (_poder/1000) % 10;
+        uint velocidad= (_poder/10**4) % 10;
+        pokemons.push(Pokemon(_name, _elemento, ataque, defensa, ataqueEspecial, defensaEspecial, velocidad));
+        uint id = pokemons.length - 1;
         pokemonToOwner[id]=msg.sender;
         ownerPokemonConunt[msg.sender]++;
+        trainerName[msg.sender]=_trainerName;
         //para generar eventos a nivel de web3
         emit NewPokemon(id, _name, _poder);
     }
 
     //keccak256 es una version de SHA3 que es una funcion Hash que retorna 256-bits en un formato de numero hexadecimal 
     //que empleamos como pseudo aleatorio, DEBERIAMOS ENCONTRAR UNA MANERA MEJOR 
-    function _randomPoder(string _str) private view returns(uint) {
-        uint rand = uint(keccak256(_str));
+    function _randomPoder(string calldata _str) private view returns(uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
         return rand % poderModulus;
     }
 
-    function createPokemon(string _name) public {
-        uint randPoder = _randomPoder(_name);
-        _createPokemon(_name, randPoder); 
+    function processElement(string memory _element) public pure returns (string memory) {
+        bytes32 elementHash = keccak256(abi.encodePacked(_element));
+
+        if (elementHash == keccak256(abi.encodePacked("Agua"))) {
+            return "Aqualis";
+        } else if (elementHash == keccak256(abi.encodePacked("Fuego"))) {
+            return "Ignis";
+        } else if (elementHash == keccak256(abi.encodePacked("Tierra"))) {
+            return "Terranox";
+        } else {
+            revert("Elemento no valido. Solo se aceptan 'Agua', 'Fuego' o 'Tierra'.");
+        }
+    }
+
+    function createPokemon(string memory _elemento, string calldata _trainerName) public {
+        require(ownerPokemonConunt[msg.sender] == 0);
+        uint randPoder = _randomPoder(_trainerName);
+         string memory name = processElement(_elemento);
+        _createPokemon(name, _elemento, randPoder, _trainerName); 
     }
     
 }
