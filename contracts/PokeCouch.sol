7332b2a9cd1be661e09d718e4077532710c3191f
MarcosUIB <marcos14122002@gmail.com>
2025-01-16 12:23:05 +0100

    Añadi obtener el pokemon de la Id valida por la función Battle y otras operaciones para crear un pokemon enemigo aleatorio

--- contracts/PokeCouch.sol
+++ contracts/PokeCouch.sol
@@ -9,10 +9,20 @@ contract PokeCouch is PokemonFactory {
   //  require(msg.sender == pokemonToOwner[pokeId]);
   //}
 
-  function battle(uint _pokemonId) view  public {
+  function battle(uint _pokemonId) view public {
     // Verificar que el ID del Pokémon sea válido
     require(_pokemonId < pokemons.length, "ID de Pokemon invalido.");
     // Verificar que el Pokémon pertenece al usuario que llama
     require(pokemonToOwner[_pokemonId] == msg.sender, "No eres el propietario de este Pokemon.");
+
+    // Obtener el Pokémon del jugador
+    Pokemon storage pokemonJugador = pokemons[_pokemonId];
+
+    // Generar un Pokémon enemigo aleatorio
+    string memory nombreEnemigo = "PokemonSalvaje";
+    uint poderEnemigo = pokemon._randomPoder("Enemigo");
+    uint ataqueEnemigo = poderEnemigo % 10;
+    uint defensaEnemigo = (poderEnemigo / 10) % 10;
+    uint velocidadEnemigo = (poderEnemigo / 100) % 10;
   }
 }
