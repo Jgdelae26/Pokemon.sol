da5d919c046db924a7e3c64a7882d8e44e8425f1
MarcosUIB <marcos14122002@gmail.com>
2025-01-16 12:00:27 +0100

    Añadí una array a la funcion de listado y un for para listar los pokemons que posee el usuario

--- contracts/PokeCouch.sol
+++ contracts/PokeCouch.sol
@@ -10,9 +10,21 @@ contract PokeCouch is PokemonFactory {
   //}
 
   // Función para listar los Pokémon de un entrenador
-  function _listPokemons() public view returns (Pokemon[] memory, uint[] memory) {
+  function _listPokemons() public view returns (Pokemon[] memory) {
     uint cantidad = ownerPokemonCount[msg.sender];
     require(cantidad > 0, "No posees ningun Pokemon.");
+
+    // Crear arrays temporales para devolver los datos
+    Pokemon[] memory misPokemon = new Pokemon[](cantidad);
+
+    uint contador = 0;
+      for (uint i = 0; i < pokemons.length; i++) {
+          if (pokemonToOwner[i] == msg.sender) {
+              misPokemon[contador] = pokemons[i];
+              contador++;
+          }
+      }
+    return (misPokemon);
   }
 
   function battle(uint _pokemonId) view  public {
