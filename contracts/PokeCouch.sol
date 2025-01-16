67f065aad9154f28d322a9fb35c64e00ac38a93a
MarcosUIB <marcos14122002@gmail.com>
2025-01-16 12:09:20 +0100

    Mejor borro la función para listar porque ya lo hace el mapping

--- contracts/PokeCouch.sol
+++ contracts/PokeCouch.sol
@@ -9,24 +9,6 @@ contract PokeCouch is PokemonFactory {
   //  require(msg.sender == pokemonToOwner[pokeId]);
   //}
 
-  // Función para listar los Pokémon de un entrenador
-  function _listPokemons() public view returns (Pokemon[] memory) {
-    uint cantidad = ownerPokemonCount[msg.sender];
-    require(cantidad > 0, "No posees ningun Pokemon.");
-
-    // Crear arrays temporales para devolver los datos
-    Pokemon[] memory misPokemon = new Pokemon[](cantidad);
-
-    uint contador = 0;
-      for (uint i = 0; i < pokemons.length; i++) {
-          if (pokemonToOwner[i] == msg.sender) {
-              misPokemon[contador] = pokemons[i];
-              contador++;
-          }
-      }
-    return (misPokemon);
-  }
-
   function battle(uint _pokemonId) view  public {
     // Verificar que el ID del Pokémon sea válido
     require(_pokemonId < pokemons.length, "ID de Pokemon invalido.");
