1ba9cc815296402cc3d1d8555b5dcec2209ed224
MarcosUIB <marcos14122002@gmail.com>
2025-01-16 11:48:16 +0100

    Cree otra funcion para listar los pokemon del usuario si tiene, en caso contrario muestra que no posse ningun pokemon

--- contracts/PokeCouch.sol
+++ contracts/PokeCouch.sol
@@ -8,6 +8,13 @@ contract PokeCouch is PokemonFactory {
   //function feed(uint pokeId)   public {
   //  require(msg.sender == pokemonToOwner[pokeId]);
   //}
+
+  // Función para listar los Pokémon de un entrenador
+  function _listPokemons() public view returns (Pokemon[] memory, uint[] memory) {
+    uint cantidad = ownerPokemonCount[msg.sender];
+    require(cantidad > 0, "No posees ningun Pokemon.");
+  }
+
   function battle(uint _pokemonId) view  public {
     // Verificar que el ID del Pokémon sea válido
     require(_pokemonId < pokemons.length, "ID de Pokemon invalido.");
