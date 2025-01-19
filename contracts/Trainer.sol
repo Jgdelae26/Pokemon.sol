8fc2196a971e50b1315116dfd30cf73ef78861f2
MarcosUIB <marcos14122002@gmail.com>
2025-01-19 17:41:04 +0100

    Require para verificar si el entrenador està registrado

--- contracts/Trainer.sol
+++ contracts/Trainer.sol
@@ -6,6 +6,7 @@ import "./Pokemon.sol";
 contract Trainer is PokemonFactory{
     // Función para registrar un entrenador
     function registrarEntrenador(address _entrenador, string calldata _name) external onlyOwner {
-
+        // Verificar si el entrenador ya está registrado
+        require(bytes(trainerName[msg.sender]).length == 0, "El entrenador ya esta registrado");
     }
 }
