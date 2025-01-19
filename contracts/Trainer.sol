4dce3321b974ca279f8840bea863610fb9d90144
MarcosUIB <marcos14122002@gmail.com>
2025-01-19 17:44:03 +0100

    Cree un push para el entrenador, y cambie _name por _trainerName

--- contracts/Trainer.sol
+++ contracts/Trainer.sol
@@ -5,8 +5,11 @@ import "./Pokemon.sol";
 
 contract Trainer is PokemonFactory{
     // Función para registrar un entrenador
-    function registrarEntrenador(address _entrenador, string calldata _name) external onlyOwner {
+    function registrarEntrenador(address _entrenador, string calldata _trainerName) external onlyOwner {
         // Verificar si el entrenador ya está registrado
         require(bytes(trainerName[msg.sender]).length == 0, "El entrenador ya esta registrado");
+
+        // Crear un nuevo entrenador y añadirlo al array
+        entrenadores.push(Entrenador(_trainerName, 0, 0, 0));
     }
 }
