aa67cf6e068e83862292a1fe2223931fd469e311
MarcosUIB <marcos14122002@gmail.com>
2025-01-19 17:24:25 +0100

    Cree una funcion para registrar entrenadores

--- contracts/Trainer.sol
+++ contracts/Trainer.sol
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.20;
 
-import "./Pokemon.sol"
+import "./Pokemon.sol";
 
-contract Trainer is PokeFactory{
-    
+contract Trainer is PokemonFactory{
+    // Función para registrar un entrenador
+    function registrarEntrenador(address _entrenador, string calldata _name) external onlyOwner {
+
+    }
 }
