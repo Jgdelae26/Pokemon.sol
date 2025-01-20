76c9320c31a867f89f509d12765e5fbc600bff26
MarcosUIB <marcos14122002@gmail.com>
2025-01-20 12:03:53 +0100

    eliminamos los datos del entrenador

--- contracts/Trainer.sol
+++ contracts/Trainer.sol
@@ -125,6 +125,11 @@ contract Trainer is PokemonFactory{
         }
         // Ajustar el array de Pokémon para compactar
         compactarArrayPokemons();
+
+        // Eliminar datos del entrenador
+        delete entrenadores[index]; // Remover del array de entrenadores
+        delete trainerName[_entrenador]; // Remover el nombre del entrenador
+        delete addressToEntrenadorIndex[_entrenador]; // Remover el índice asociado
     }
 
     // Función para compactar el array de Pokémon después de eliminaciones
