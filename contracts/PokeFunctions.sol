0541226d4b3319806618eeb395e5c2c0927944ed
Jgdelae26 <jgdelae26@gmail.com>
2025-01-22 17:57:34 +0100

    Puesta a punto

--- contracts/PokeFunctions.sol
+++ contracts/PokeFunctions.sol
@@ -21,35 +21,6 @@ contract PokeFunctions is PokeCouch {
       pokemons[_pokemonId].level++;
     }
   
-    //Función para ver los poquemons que tiene una direccion en concreto. No es la forma que seria mas practica pero si la mas eficiente, ya que
-    // trabajamos con un array nuevo de tipo memory y no modificamos ningun array de los que tenemos en storage directamente entonces este array en memory se
-    //creara cada vez que se llame la funcion y se recorrera el array en storage buscando los pokemon de la direeccion. Poco eficiente pero barat.
-    //Mejor explicado en https://cryptozombies.io/es/lesson/3/chapter/12
-    function getPokemonByOwner(address _owner)
-        external
-        view
-        returns (uint256[] memory)
-    {
-        uint256[] memory result = new uint256[](ownerPokemonCount[_owner]);
-        uint256 counter = 0;
-        for (uint256 i = 0; i < pokemons.length; i++) {
-            if (pokemonToOwner[i] == _owner) {
-                result[counter] = i;
-                counter++;
-            }
-        }
-        return result;
-    }
-
-    //Dispara el timepo de recuperacion de un pokemon
-    function _disparadorDeRecu(Pokemon storage _pokemon) internal {
-        _pokemon.readyTime = uint32(block.timestamp + tiempoRecu);
-    }
-
-    //Comprueba que el pokemon este recuperado
-    function _isReady(Pokemon storage _pokemon) internal view returns (bool) {
-        return (_pokemon.readyTime <= block.timestamp);
-    }
 
     //Comprueba que el nivel del pokemon es superior o igual al que se le indica
     modifier aboveLevel(uint256 _level, uint256 _pokemonId) {
@@ -67,18 +38,5 @@ contract PokeFunctions is PokeCouch {
         pokemons[_pokemonId].name = _newName;
     }
 
-    //Aumento de alguna de las stats dentro del struc estas estan indexadas y se puede acceder a ellas mediante
-    function _modificacionEstats(Pokemon storage _myPokemon, uint8 indexStat, uint8 _cantidadSubida) internal {
-        if (indexStat == 1) {
-            _myPokemon.ataque = _myPokemon.ataque + _cantidadSubida;
-        } else if (indexStat == 2) {
-            _myPokemon.defensa = _myPokemon.defensa + _cantidadSubida;
-        } else if (indexStat == 3) {
-            _myPokemon.ataqueEspecial = _myPokemon.ataqueEspecial + _cantidadSubida;
-        } else if (indexStat == 4) {
-          _myPokemon.defensaEspecial = _myPokemon.defensaEspecial + _cantidadSubida;
-        } else if (indexStat == 5) {
-          _myPokemon.velocidad = _myPokemon.velocidad + _cantidadSubida;
-        }
-    }
+
 }
